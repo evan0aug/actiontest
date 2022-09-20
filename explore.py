f843e17c415c94cb957a273c0b33f9a71f7c7631
evan0aug <evan@augmentcode.com>
2022-09-20 22:30:18 +0000

    Another play commit

--- explore.py
+++ explore.py
@@ -1,27 +1,28 @@
-''' Use numpy for handling stats '''
-import numpy as np 
+""" This a simple file for Evan to play with various tools without messing up Augment """
 
-STATS = [
-    "STR",
-    "DEX",
-    "CON",
-    "INT",
-    "WIS",
-    "CHA"
-    ]
+import sys
+import time
+
+import numpy as np
+
+STATS = ["STR", "DEX", "CON", "INT", "WIS", "CHA"]
 
 
 def generate_stats() -> np.ndarray:
-    """ generate stats?"""
+    """generate stats?"""
     for stat in STATS:
-        stats = np.random.randint(1,7, (1,4))
+        stats = np.random.randint(1, 7, (1, 4))
         stats = np.delete(stats, stats.argmin())
         print(stat + ": " + str(stats) + ": " + str(stats.sum()))
     return stats
 
+
 def main():
-    """ Do something useful """
+    """Do something useful"""
     generate_stats()
 
 
+now = time.localtime()
+print(now)
 main()
+sys.exit()
